ddcb721c13f6d1a0c1bb3e2dfbf40af2fdb9fe00
shashank <it17136shashank@gmail.com>
2023-05-06 13:22:48 +0530

    working on jpa callbacks

--- src/main/java/com/backend/hibernateadvance/compositeKey/model/entity/Person.java
+++ src/main/java/com/backend/hibernateadvance/compositeKey/model/entity/Person.java
@@ -1,5 +1,6 @@
 package com.backend.hibernateadvance.compositeKey.model.entity;
 
+import com.backend.hibernateadvance.attributeConverter.PersonCallbacks;
 import com.backend.hibernateadvance.attributeConverter.PersonNameConverter;
 import com.backend.hibernateadvance.compositeKey.model.PersonName;
 import com.backend.hibernateadvance.compositeKey.model.idClass.PersonPKId;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.Table;
@@ -19,6 +21,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @IdClass(PersonPKId.class)
 @Table(name = "person_tbl")
+@EntityListeners(PersonCallbacks.class)
 public class Person {
     @Id
     private Long personId;
---
+++ src/main/java/com/backend/hibernateadvance/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+//package com.backend.hibernateadvance.config;
+//
+//import com.backend.hibernateadvance.attributeConverter.StringToPersonPKIdConverter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.format.FormatterRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+////@Configuration
+////public class WebMvcConfig implements WebMvcConfigurer {
+////
+////    @Autowired
+////    private StringToPersonPKIdConverter stringToPersonPKIdConverter;
+////
+////    @Override
+////    public void addFormatters(FormatterRegistry registry) {
+////        registry.addConverter(stringToPersonPKIdConverter);
+////    }
+////}
